ec14712cf94b743aa6b1e6fca8b5991bd34e68df
Owen-l4 <b0925138932@gmail.com>
2021-09-21 15:37:16 +0800

    add try to youtube together function

--- functions/YoutubeTogether.py
+++ functions/YoutubeTogether.py
@@ -7,9 +7,13 @@ class YoutubeTogether(commands.Cog):
         self.togetherControl = DiscordTogether(bot)
     
     @commands.command()
-    async def start(self, ctx):
-        link = await self.togetherControl.create_link(ctx.author.voice.channel.id, 'youtube')
-        await ctx.send(f"Click the blue link!\n{link}")
+    async def YoutubeTogether(self, ctx):
+        try:
+            link = await self.togetherControl.create_link(ctx.author.voice.channel.id, 'youtube')
+            await ctx.message.delete()
+            await ctx.send(f"click the link to join the\n{link}")
+        except:
+            await ctx.send("you are not in a voice channel")
 
 def setup(bot):
     bot.add_cog(YoutubeTogether(bot))
